851527b62a1c8f9d60e4e96ae9a5d8337d7e48af
handy <handy1989@qq.com>
2014-05-15 15:33:56 -0700

    addfiles

--- epoll_socket.cpp
+++ epoll_socket.cpp
@@ -5,9 +5,9 @@ EpollSocket::EpollSocket()
 
 }
 
-EpollSocket::EpollSocket(netaddres_info_t *netaddr, const int& addr_num)
+EpollSocket::EpollSocket(netaddres_info_t *netaddr, const int& addr_num, const int& wait_time_ms)
 {
-    init(netaddr, addr_num);
+    init(netaddr, addr_num, wait_time_ms);
 }
 
 EpollSocket::~EpollSocket()
@@ -26,8 +26,10 @@ EpollSocket::~EpollSocket()
     delete []m_net_addr;
 }
 
-void EpollSocket::init(netaddres_info_t *netaddr, const int& addr_num)
+void EpollSocket::init(netaddres_info_t *netaddr, const int& addr_num, const int& wait_time_ms)
 {
+    LOG(INFO) << "EpollSocket init";
+    m_wait_time_ms = wait_time_ms
     m_listen_num = addr_num;
     pthread_mutex_init(m_mutex, NULL);
     m_epoll_fd = epoll_create(EPOLL_SIZE);
@@ -46,7 +48,79 @@ void EpollSocket::init(netaddres_info_t *netaddr, const int& addr_num)
     {
        m_sock_info[i].m_listen_port = 0;
        m_sock_info[i].m_port = 0;
+       m_sock_info[i].m_time_ms = 0;
+       m_sock_info[i].m_addr = 0;
     }
     m_sock_num = 0;
 }
 
+int EpollSocket::waitAvailableSocket()
+{
+    m_available_socket_num = epoll_wailt(m_epoll_fd, m_events, EVENTS_SIZE, m_wait_time_ms);
+    LOG(INFO) << "waitAvailableSocket: " << m_available_socket_num;
+    return m_available_socket_num;
+}
+
+int getSockCount()
+{
+    LOG(INFO) << "getSocketCount:" << m_socket_num;
+    return m_socket_num;
+}
+
+bool EpollSocket::removeSocket(int sock_fd)
+{
+    LOG(INFO) << "remove fd:" << sock_fd;
+    struct epoll_event ev;
+    ev.data.fd = sock_fd;
+    bool ret = false;
+    pthread_mutex_lock(m_mutex);
+    ret = epoll_ctl(m_epoll_fd, EPOLL_CTL_DEL, sock_fd, &ev);
+    if (success)
+    {
+        --m_socket_num;
+        if (sock_fd >= 0 && sock_fd < EPOLL_SIZE)
+        {
+            m_socket_info[sock_fd].m_time_ms = 0;
+        }
+    }
+    pthread_mutex_destroy(m_mutex);
+    LOG(INFO) << "remove fd: " << sock_fd << " ret: " << ret;
+    return ret;
+}
+
+bool EpollSocket::add(int sock_fd, int listen_port, int port, int sock_addr)
+{
+    LOG(INFO) << "add fd: " << sock_fd;
+    struct epoll_event ev;
+    ev.data.fd = sock_fd;
+    ev.events = EPOLL_IN | EPOLL_ET;
+    if (sock_fd < 0 || sock_fd >= EPOLL_SIZE)
+    {
+        LOG(ERROR) << "sock_fd invalid: " << sock_fd;
+        return false;
+    }
+    if (!setNonblock(sock_fd))
+    {
+        LOG(ERROR) << "setNonblock failed: " << sock_fd;
+        return false;
+    }
+    int buf_size = 1024*1024;
+    setsockopt(sock_fd, SOL_SOCKET, SO_RECVBUF, &buf_size, sizeof(buf_size));
+    bool ret = false;
+    pthread_mutex_lock(m_mutex);
+    m_sock_info[sock_fd].m_port = port;
+    m_socket_info[sock_fd].m_addr = sock_addr;
+
+    ret = (epoll_ctl(sock_fd, EPOLL_CTL_ADD, sock_fd, &ev) == 0);
+    if (ret)
+    {
+        ++m_socket_num;
+    }
+    else
+    {
+        m_socket_info[sock_fd].m_time_ms = 0;
+    }
+    pthread_mutex_unlock(m_mutex);
+    LOG(INFO) << "add fd: " << sock_fd << " ret: " << ret;
+    return ret;
+}
--- epoll_socket.h
+++ epoll_socket.h
@@ -12,6 +12,8 @@ typedef _sock_info_t
 {
     int m_insten_port;
     int m_port;
+    int m_addr;
+    int m_time_ms;
     std::string m_recv_buf;
 
 }sock_info_t;
@@ -29,22 +31,26 @@ class EpollSocket
     static const int EVENTS_SIZE = EPOLL_SIZE;
 public:
     EpollSocket();
-    EpollSocket(netaddres_info_t *netaddr, const int& addr_num);
+    EpollSocket(netaddres_info_t *netaddr, const int& addr_num, const int& wait_time_ms);
     ~EpollSocket();
-    void init(netaddres_info_t *netaddr, const int& addr_num);
-    int wait_available_socket();
-    bool remove_socket(int sock_fd);
+    void init(netaddres_info_t *netaddr, const int& addr_num, const int& wait_time_ms);
+    int waitAvailableSocket();
+    int getSocketCount();
+    bool removeSocket(int sock_fd);
     bool add(int sock_fd, int listen_port, int port, int sock_addr);
-    bool bind_port();
-    bool listen_port();
-    bool set_nonblock();
+    bool bindPort();
+    bool listenPort();
+    bool setNonblock();
 protected:
-    int get_port(int sock_fd);
-    int get_addr(int sock_fd);
+    int getPort(int sock_fd);
+    int getAddr(int sock_fd);
 
 private:
     int m_listen_num;
     int m_epoll_fd;
+    int m_socket_num;
+    int m_available_socket_num;
+    int m_wait_time_ms;
     int *m_socket;
     netaddres_info_t *m_net_addr;
     pthread_mutex_t m_mutex;
